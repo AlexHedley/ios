3774c2aadec11753c42670645b107a0b4f5b8863
Alex Hedley <alex.hedley@me.com>
2023-11-24 13:09:28 +0000

    Initial site with apps

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# ios
+# iOS
+
 iOS Apps
+
+## Run
+
+`cd src`
+
+`py -m http.server`
+
+- [local](http://localhost:8000/)
---
+++ src/js/app.js
@@ -0,0 +1,38 @@
+var app = angular.module('app', []);
+app.controller('controller', function ($scope, $http, $q, $filter) {
+
+    $scope.apps = [];
+
+    $scope.init = function () {
+        getData();
+    }
+
+    getData = () =>  {
+        var file = 'data/apps.json';
+
+        $http.get(file)
+            .then(function(response) {
+                $scope.apps = response.data.apps;
+            });
+    };
+
+    $scope.openRepository = (app) => {
+        window.open(app.code);
+    }
+
+    $scope.openSite = (app) => {
+        window.open(app.site);
+    }
+
+    $scope.openAppSite = (app) => {
+        window.open(app.appSite);
+    }
+
+    $scope.init();
+});
+
+app.filter('toDate', function() {
+    return function(items) {
+        return new Date(items);
+    };
+});
